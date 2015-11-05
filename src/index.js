f1f8718f67e8bf0690de6c0f19a81ee3656f38aa
Rich Harris <richard.a.harris@gmail.com>
2015-11-05 16:51:23 -0500

    generate named exports for valid keys - closes #1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-export default function json () {
-	return {
-		transform ( code, id ) {
-			if ( id.slice( -5 ) !== '.json' ) return null;
+import { createFilter, makeLegalIdentifier } from 'rollup-pluginutils';
 
-			return {
-				code: 'export default ' + code + ';',
-				map: { mappings: '' }
-			};
+export default function json ( options = {} ) {
+	const filter = createFilter( options.include, options.exclude );
+
+	return {
+		transform ( json, id ) {
+			if ( id.slice( -5 ) !== '.json' ) return null;
+			if ( !filter( id ) ) return null;
+
+			let code;
+
+			if ( json[0] !== '{' ) {
+				code = `export default ${json};`;
+			} else {
+				const data = JSON.parse( json );
+
+				const validKeys = [];
+				const invalidKeys = [];
+
+				Object.keys( data ).forEach( key => {
+					if ( key === makeLegalIdentifier( key ) ) {
+						validKeys.push( key );
+					} else {
+						invalidKeys.push( key );
+					}
+				});
+
+				const namedExports = validKeys
+					.map( key => `export var ${key} = ${JSON.stringify( data[ key ] )};` )
+					.join( '\n' );
+
+				const defaultExportRows = validKeys.map( key => `${key}: ${key}` )
+					.concat( invalidKeys.map( key => `"${key}": ${JSON.stringify( data[ key ] )}` ) );
+
+				const defaultExports = `export default {\n\t${defaultExportRows.join( ',\n\t' )}\n};`;
+
+				code = `${namedExports}\n${defaultExports}`;
+			}
+
+			return { code, map: { mappings: '' } };
 		}
 	};
 }
--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ var assert = require( 'assert' );
 var rollup = require( 'rollup' );
 var json = require( '..' );
 
+require( 'source-map-support' ).install();
+
 process.chdir( __dirname );
 
 describe( 'rollup-plugin-json', function () {
@@ -15,6 +17,23 @@ describe( 'rollup-plugin-json', function () {
 
 			var fn = new Function( 'assert', code );
 			fn( assert );
+		});
+	});
+
+	it( 'generates named exports', function () {
+		return rollup.rollup({
+			entry: 'samples/named/main.js',
+			plugins: [ json() ]
+		}).then( function ( bundle ) {
+			var generated = bundle.generate({ format: 'cjs' });
+			var code = generated.code;
+
+			var exports = {};
+			var fn = new Function( 'exports', code );
+			fn( exports );
+
+			assert.equal( exports.version, '1.33.7' );
+			assert.equal( code.indexOf( 'this-should-be-excluded' ), -1, 'should exclude unused properties' );
 		});
 	});
 });
