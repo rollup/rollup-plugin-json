1c850bf48bb2319f32e22d0f84c569915b38eb61
Rich Harris <richard.a.harris@gmail.com>
2017-06-03 09:53:11 -0400

    lint

--- test/test.js
+++ test/test.js
@@ -1,41 +1,41 @@
-var fs = require('fs');
-var assert = require('assert');
-var rollup = require('rollup');
-var json = require('..');
-var npm = require('rollup-plugin-node-resolve');
+const fs = require('fs');
+const assert = require('assert');
+const rollup = require('rollup');
+const json = require('..');
+const npm = require('rollup-plugin-node-resolve');
 
 require('source-map-support').install();
 
 process.chdir(__dirname);
 
-describe('rollup-plugin-json', function() {
-	it('converts json', function() {
+describe('rollup-plugin-json', () => {
+	it('converts json', () => {
 		return rollup
 			.rollup({
 				entry: 'samples/basic/main.js',
 				plugins: [json()]
 			})
-			.then(function(bundle) {
-				var generated = bundle.generate();
-				var code = generated.code;
+			.then(bundle => {
+				const generated = bundle.generate();
+				const code = generated.code;
 
-				var fn = new Function('assert', code);
+				const fn = new Function('assert', code);
 				fn(assert);
 			});
 	});
 
-	it('generates named exports', function() {
+	it('generates named exports', () => {
 		return rollup
 			.rollup({
 				entry: 'samples/named/main.js',
 				plugins: [json()]
 			})
-			.then(function(bundle) {
-				var generated = bundle.generate({ format: 'cjs' });
-				var code = generated.code;
+			.then(bundle => {
+				const generated = bundle.generate({ format: 'cjs' });
+				const code = generated.code;
 
-				var exports = {};
-				var fn = new Function('exports', code);
+				const exports = {};
+				const fn = new Function('exports', code);
 				fn(exports);
 
 				assert.equal(exports.version, '1.33.7');
@@ -47,24 +47,24 @@ describe('rollup-plugin-json', function() {
 			});
 	});
 
-	it('resolves extensionless imports in conjunction with npm plugin', function() {
+	it('resolves extensionless imports in conjunction with npm plugin', () => {
 		return rollup
 			.rollup({
 				entry: 'samples/extensionless/main.js',
 				plugins: [npm({ extensions: ['.js', '.json'] }), json()]
 			})
-			.then(function(bundle) {
-				var generated = bundle.generate();
-				var code = generated.code;
+			.then(bundle => {
+				const generated = bundle.generate();
+				const code = generated.code;
 
-				var fn = new Function('assert', code);
+				const fn = new Function('assert', code);
 				fn(assert);
 			});
 	});
 
-	it('generates a correct fake AST', function() {
-		var pkg = fs.readFileSync('samples/ast/package.json', 'utf-8');
-		var transformed = json().transform(pkg, 'package.json');
+	it('generates a correct fake AST', () => {
+		const pkg = fs.readFileSync('samples/ast/package.json', 'utf-8');
+		const transformed = json().transform(pkg, 'package.json');
 
 		assert.deepEqual(transformed.ast, {
 			type: 'Program',
@@ -250,9 +250,9 @@ describe('rollup-plugin-json', function() {
 		});
 	});
 
-	it('generates a correct fake AST with preferConst', function() {
-		var pkg = fs.readFileSync('samples/ast/package.json', 'utf-8');
-		var transformed = json({ preferConst: true }).transform(
+	it('generates a correct fake AST with preferConst', () => {
+		const pkg = fs.readFileSync('samples/ast/package.json', 'utf-8');
+		const transformed = json({ preferConst: true }).transform(
 			pkg,
 			'package.json'
 		);
@@ -441,22 +441,22 @@ describe('rollup-plugin-json', function() {
 		});
 	});
 
-	it('handles JSON objects with no valid keys (#19)', function() {
+	it('handles JSON objects with no valid keys (#19)', () => {
 		return rollup
 			.rollup({
 				entry: 'samples/no-valid-keys/main.js',
 				plugins: [json()]
 			})
-			.then(function(bundle) {
-				var generated = bundle.generate();
-				var code = generated.code;
+			.then(bundle => {
+				const generated = bundle.generate();
+				const code = generated.code;
 
-				var fn = new Function('assert', code);
+				const fn = new Function('assert', code);
 				fn(assert);
 			});
 	});
 
-	it('uses custom indent string', function() {
+	it('uses custom indent string', () => {
 		assert.equal(
 			json({ indent: '  ' }).transform(
 				read(`samples/custom-indent/input.json`),
